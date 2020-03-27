37bdccc04821c86f49ab618f3145b49d76f64ded
nacho.borras <nacho.borras@oramon.nl>
2020-03-27 15:31:08 +0100

    Removing commented code

--- src/index.js
+++ src/index.js
@@ -8,34 +8,9 @@ const resolvers = require('./resolvers');
 
 const EntryAPI = require('./datasources/entry');
 const UserAPI = require('./datasources/user');
-const internalEngineDemo = require('./engine-demo');
+// const internalEngineDemo = require('./engine-demo');
 
 const store = createStore();
-
-// const server = new ApolloServer({
-//   context: async ({ req }) => {
-//     // simple auth check on every request
-//     const auth = (req.headers && req.headers.authorization) || '';
-//     const email = Buffer.from(auth, 'base64').toString('ascii');
-//     if (!isEmail.validate(email)) return { user: null };
-
-//     // find a user by their email
-//     const users = await store.users.findOrCreate({ where: { email } });
-//     const user = (users && users[0]) || null;
-
-//     return { user: { ...user.dataValues } };
-//   },
-//   dataSources: () => ({
-//     entryAPI: new EntryAPI({ store }),
-//     userAPI: new UserAPI({ store }),
-//   }),
-//   resolvers,
-//   typeDefs,
-//   engine: {
-//     apiKey: process.env.ENGINE_API_KEY,
-//     ...internalEngineDemo,
-//   },
-// });
 
 const server = new ApolloServer({
   dataSources: () => ({
@@ -54,10 +29,6 @@ const app = express();
 server.applyMiddleware({ app });
 
 app.use(express.static("app/public"));
-
-// server.use()
-
-// server.use()
 
 app.listen({ port: process.env.PORT || 4000 }, () => {
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
