123039b299621ec2dcb347ca7155ee03ac50bc50
nacho.borras <nacho.borras@oramon.nl>
2020-03-27 13:44:27 +0100

    Adding express server

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer } = require('apollo-server-express');
 const { createStore } = require('./utils');
+const express = require("express")
 
 const isEmail = require('isemail');
 const typeDefs = require('./schema');
@@ -11,31 +12,50 @@ const internalEngineDemo = require('./engine-demo');
 
 const store = createStore();
 
+// const server = new ApolloServer({
+//   context: async ({ req }) => {
+//     // simple auth check on every request
+//     const auth = (req.headers && req.headers.authorization) || '';
+//     const email = Buffer.from(auth, 'base64').toString('ascii');
+//     if (!isEmail.validate(email)) return { user: null };
+
+//     // find a user by their email
+//     const users = await store.users.findOrCreate({ where: { email } });
+//     const user = (users && users[0]) || null;
+
+//     return { user: { ...user.dataValues } };
+//   },
+//   dataSources: () => ({
+//     entryAPI: new EntryAPI({ store }),
+//     userAPI: new UserAPI({ store }),
+//   }),
+//   resolvers,
+//   typeDefs,
+//   engine: {
+//     apiKey: process.env.ENGINE_API_KEY,
+//     ...internalEngineDemo,
+//   },
+// });
+
 const server = new ApolloServer({
-  context: async ({ req }) => {
-    // simple auth check on every request
-    const auth = (req.headers && req.headers.authorization) || '';
-    const email = Buffer.from(auth, 'base64').toString('ascii');
-    if (!isEmail.validate(email)) return { user: null };
-
-    // find a user by their email
-    const users = await store.users.findOrCreate({ where: { email } });
-    const user = (users && users[0]) || null;
-
-    return { user: { ...user.dataValues } };
-  },
   dataSources: () => ({
     entryAPI: new EntryAPI({ store }),
     userAPI: new UserAPI({ store }),
   }),
-  resolvers,
   typeDefs,
-  engine: {
-    apiKey: process.env.ENGINE_API_KEY,
-    ...internalEngineDemo,
-  },
+  resolvers,
+  context: { store }
 });
 
-server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-  console.log(`Server ready at ${url}`);
+const app = express();
+server.applyMiddleware({ app });
+
+app.use(express.static("app/public"));
+
+// server.use()
+
+// server.use()
+
+app.listen({ port: process.env.PORT || 4000 }, () => {
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
 });
